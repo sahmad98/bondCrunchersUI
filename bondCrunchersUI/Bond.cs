8add1b4639989f2a749a293c6df65fb03cb3fa26
John Doe <saleem.xavier@gmail.com>
2016-08-04 13:36:50 +0530

    Working GET POST request

---
+++ bondCrunchersUI/Bond.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Web.Script.Serialization;
+
+namespace bondCrunchersUI
+{
+    [DataContract]
+    class Bond
+    {
+        public string ISIN { get; private set; }
+    }
+}
--- bondCrunchersUI/MainWindow.xaml.cs
+++ bondCrunchersUI/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.Net;
 using System.IO;
 using System.Runtime.Serialization.Json;
+using System.Web.Script.Serialization;
+
 namespace bondCrunchersUI
 {
     /// <summary>
@@ -26,15 +28,28 @@ namespace bondCrunchersUI
         {
             InitializeComponent();
         }
-
+        JavaScriptSerializer js = new JavaScriptSerializer();
         private void BookTrade(object sender, RoutedEventArgs e)
         {
             WebClient web = new WebClient();
             string uri = "http://192.168.66.53:8080/EBondTraderWeb/rest/EBond";
             web.BaseAddress = uri;
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TestClass));
-            TestClass t1 = (TestClass)serializer.ReadObject(web.OpenRead(uri));
-            bondList.Items.Add(t1);
+            //DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TestClass));
+            
+            //TestClass t1 = (TestClass)serializer.ReadObject(web.OpenRead(uri));
+            //bondList.Items.Add(t1);
+            TestClass t1 = new TestClass("IN123", "mybond");
+            /*DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(TestClass));
+            MemoryStream ms = new MemoryStream();
+            json.WriteObject(ms, t1);
+            ms.Position = 0;
+            StreamReader sr = new StreamReader(ms);*/
+            string json = js.Serialize(t1);
+            web.Headers.Add(HttpRequestHeader.ContentType,"application/json");
+            web.UploadString(uri, "POST", json);   
+                    
         }
+        //JavaScriptSerializer json = new JavaScriptSerializer();
+
     }
 }
